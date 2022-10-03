8ada0bc8b3b3fb38f8ec5dff9807fb4a3e03e144
Conrad Wood <cnw@conradwood.net>
2022-10-03 11:43:03 +0100

    made dirwalker and dirhasher consistent
    Change-Id: I5f9af656b768c8d00a68e1b7868695449189dc2b

--- src/golang.conradwood.net/go-easyops/utils/dirhash.go
+++ src/golang.conradwood.net/go-easyops/utils/dirhash.go
@@ -5,10 +5,6 @@ import (
 	"fmt"
 	"hash"
 	"io/ioutil"
-	"os"
-	"path/filepath"
-	"sort"
-	"strings"
 )
 
 type DirHasher struct {
@@ -23,89 +19,27 @@ func DirHash(ddir string) (string, error) {
 	if ddir == "" {
 		return "", fmt.Errorf("no config dir")
 	}
-	ddir = strings.TrimSuffix(ddir, "/")
 	dh := &DirHasher{root: ddir, hasher: sha256.New()}
-	err := dh.WalkDir("/")
+	err := DirWalk(ddir, dh.AddEntry)
 	if err != nil {
 		return "", err
 	}
 	sum := fmt.Sprintf("%x", dh.hasher.Sum(nil))
 	return sum, nil
 }
-
-// path is relative to "root"
-func (dh *DirHasher) WalkDir(path string) error {
-	path = strings.TrimPrefix(path, "/")
-	fpath := fmt.Sprintf("%s/%s", dh.root, path)
-	entries, err := ioutil.ReadDir(fpath)
+func (dh *DirHasher) AddEntry(root string, relname string) error {
+	_, err := dh.hasher.Write([]byte(relname))
 	if err != nil {
 		return err
 	}
-	sort.Slice(entries, func(i, j int) bool {
-		return entries[i].Name() < entries[j].Name()
-	})
-	// hash all filenames
-	for _, e := range entries {
-		relname := path + "/" + e.Name()
-		//		fmt.Printf("file: \"%s\"\n", relname)
-		_, err = dh.hasher.Write([]byte(relname))
-		if err != nil {
-			return err
-		}
-	}
-
-	// do files first
-	for _, e := range entries {
-		if !e.Mode().IsRegular() {
-			continue
-		}
-		fname := fpath + "/" + e.Name()
-		b, err := ioutil.ReadFile(fname)
-		if err != nil {
-			return err
-		}
-		_, err = dh.hasher.Write(b)
-		if err != nil {
-			return err
-		}
-	}
-	// do dirs now
-	for _, e := range entries {
-		if !e.IsDir() {
-			continue
-		}
-		fname := e.Name()
-		err := dh.WalkDir(path + "/" + fname)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// MD5All reads all the files in the file tree rooted at root and returns a map
-// from file path to the MD5 sum of the file's contents.  If the directory walk
-// fails or any read operation fails, MD5All returns an error.
-func SHAAll(root string) ([]string, error) {
-	var m []string
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.Mode().IsRegular() {
-			return nil
-		}
-		data, err := ioutil.ReadFile(path)
-		if err != nil {
-			return err
-		}
-		data = append(data, ([]byte(path))...) // append the filename to data, so that change of filename also changes dirhash
-		x := sha256.Sum256(data)
-		m = append(m, fmt.Sprintf("%x", x))
-		return nil
-	})
+	b, err := ioutil.ReadFile(root + "/" + relname)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return m, nil
+	_, err = dh.hasher.Write(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
