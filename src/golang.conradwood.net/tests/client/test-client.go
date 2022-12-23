d0e439dc88bfb49bcf8342ce825449bda14d1258
Conrad Wood <cnw@conradwood.net>
2022-12-23 19:41:27 +0000

    .
    Change-Id: I0c66cc52fe50a19be154bee9b5b9b20316d8cd6d

---
+++ src/golang.conradwood.net/tests/client/test-client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"golang.conradwood.net/apis/common"
+	"golang.conradwood.net/apis/helloworld"
+	"golang.conradwood.net/go-easyops/auth"
+	"golang.conradwood.net/go-easyops/authremote"
+	"golang.conradwood.net/go-easyops/client"
+	"golang.conradwood.net/go-easyops/utils"
+	"time"
+)
+
+func main() {
+	flag.Parse()
+	fmt.Printf("go-easyops test client\n")
+	ctx := authremote.Context()
+
+	fmt.Printf("Pinging with default client...\n")
+	started := time.Now()
+	r, err := helloworld.GetHelloWorldClient().Ping(ctx, &common.Void{})
+	utils.Bail("failed to ping", err)
+	fmt.Printf("Pinged (%0.2fs), User=%s, Service=%s\n", time.Since(started).Seconds(), auth.Description(r.CallingUser), auth.Description(r.CallingService))
+
+	fmt.Printf("Pinging with lookup...\n")
+	started = time.Now()
+	con := client.Connect("helloworld.HelloWorld")
+	c := helloworld.NewHelloWorldClient(con)
+	_, err = c.Ping(ctx, &common.Void{})
+	utils.Bail("failed to ping", err)
+	fmt.Printf("Pinged (%0.2fs), User=%s, Service=%s\n", time.Since(started).Seconds(), auth.Description(r.CallingUser), auth.Description(r.CallingService))
+	/*
+		fmt.Printf("Pinging stream...\n")
+		psreq := &helloworld.PingStreamRequest{DelayInMillis: 500}
+		srv, err := helloworld.GetHelloWorldClient().PingStream(ctx, psreq)
+		utils.Bail("failed to set up pingstream", err)
+		for {
+			pr, err := srv.Recv()
+			if err != nil {
+				fmt.Printf("error received: %s\n", err)
+				break
+			}
+			fmt.Printf("Received Sequence %d\n", pr.SequenceNumber)
+		}
+	*/
+}
