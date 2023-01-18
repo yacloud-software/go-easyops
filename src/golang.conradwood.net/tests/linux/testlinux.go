f7b20be02848d8963d8080647a3c1328ac039521
Conrad Wood <cnw@conradwood.net>
2023-01-18 19:42:43 +0000

    automatic commit
    Change-Id: Ia193610d80259f8fff1d508fbfee18ca6c2f8996

--- src/golang.conradwood.net/tests/linux/testlinux.go
+++ src/golang.conradwood.net/tests/linux/testlinux.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
 	l "golang.conradwood.net/go-easyops/linux"
 	"golang.conradwood.net/go-easyops/utils"
+	"strings"
+	"time"
 )
 
 func main() {
 	lin := l.New()
 	fmt.Printf("My IP: %s\n", lin.MyIP())
+	run([]string{"/bin/true"})
+	run([]string{"sleep", "100"})
+	time.Sleep(time.Duration(6) * time.Second)
 	//	TestExecuteContainer()
 }
-
+func run(com []string) {
+	fmt.Printf("executing \"%s\"...", strings.Join(com, " "))
+	lin := l.New()
+	out, err := lin.SafelyExecute(com, nil)
+	if err != nil {
+		fmt.Printf("Output:\n%s\n", out)
+		utils.Bail("failed to execute", err)
+	}
+	fmt.Printf("Done\n")
+}
 func TestExecuteContainer() {
 	panic("no containers yet")
 }
