e6dfbb223ba8df3e03a1fd0b64b0e50bc4f64b4e
Conrad Wood <cnw@conradwood.net>
2023-06-04 08:10:53 +0100

    also call at start()
    Change-Id: I71fb79158f971d40d28dcdb343449461164e855e

--- src/golang.conradwood.net/go-easyops/utils/periodictimer.go
+++ src/golang.conradwood.net/go-easyops/utils/periodictimer.go
@@ -6,9 +6,7 @@ import (
 	"time"
 )
 
-var (
-	ptlock sync.Mutex
-)
+var ()
 
 type PeriodicTimer struct {
 	secs                  []uint32 // sorted as Largest first
@@ -18,6 +16,9 @@ type PeriodicTimer struct {
 	stop_request          bool // true if thread is meant to exit
 	wait_chan             chan bool
 	lastSuccessfulRunSecs uint32
+	wasRunAtStart         bool // true if it was run at least once
+	lock                  sync.Mutex
+	runLock               sync.Mutex
 }
 
 /*
@@ -25,6 +26,7 @@ A "PeriodicTimer" executes a callback at certain intervals over a certain period
 For example, a periodictimer, defined as NewPeriodicTimer([]uint32{20,15,5}) will run for 20 seconds and call the callback
 after 5 seconds, 10 seconds and 20 seconds.
 The callback will be retried every second if it returns an error until it returns no error
+callback will also be called each time "Start()" is called.
 */
 func NewPeriodicTimer(secs []uint32, cb func(pt *PeriodicTimer, secsLapsed uint32) error) *PeriodicTimer {
 	if len(secs) == 0 {
@@ -37,20 +39,26 @@ func NewPeriodicTimer(secs []uint32, cb func(pt *PeriodicTimer, secsLapsed uint3
 	return pt
 }
 func (pt *PeriodicTimer) Start() {
-	ptlock.Lock()
+	pt.lock.Lock()
 	pt.started = time.Now()
 	pt.lastSuccessfulRunSecs = 0
 	pt.stop_request = false
+	pt.wasRunAtStart = false
 	if !pt.thread_running {
 		go pt.timerLoop()
 		pt.thread_running = true
 	}
-	ptlock.Unlock()
+	err := pt.run_callback(0)
+	if err == nil {
+		pt.wasRunAtStart = true
+	}
+	pt.lock.Unlock()
+
 }
 func (pt *PeriodicTimer) Stop() {
-	ptlock.Lock()
+	pt.lock.Lock()
 	pt.stop_request = true
-	ptlock.Unlock()
+	pt.lock.Unlock()
 }
 
 // wait for timer to either stop or expire
@@ -72,13 +80,13 @@ func (pt *PeriodicTimer) timerLoop() {
 		}
 
 	}
-	ptlock.Lock()
+	pt.lock.Lock()
 	pt.wait_chan <- true
 	pt.stop_request = false
 	if !pt.thread_running {
 		pt.thread_running = false
 	}
-	ptlock.Unlock()
+	pt.lock.Unlock()
 }
 func (pt *PeriodicTimer) is_running_for_as_long_as_need_be() bool {
 	rs := time.Since(pt.started)
@@ -100,14 +108,21 @@ func (pt *PeriodicTimer) checkTime() {
 		period = r
 		break
 	}
-	if period == pt.lastSuccessfulRunSecs {
+	if period == pt.lastSuccessfulRunSecs && pt.wasRunAtStart {
 		return
 	}
-	err := pt.callback(pt, uint32(period))
+
+	err := pt.run_callback(period)
 	if err == nil {
 		pt.lastSuccessfulRunSecs = period
+		pt.wasRunAtStart = true
 	}
-
+}
+func (pt *PeriodicTimer) run_callback(period uint32) error {
+	pt.runLock.Lock()
+	defer pt.runLock.Unlock()
+	err := pt.callback(pt, uint32(period))
+	return err
 }
 func (pt *PeriodicTimer) Secs() []uint32 {
 	return pt.secs
