ceb3bff2226c9c9a5016603df5e207ee935ef89c
Conrad Wood <cnw@conradwood.net>
2023-06-18 11:43:59 +0100

    added debug token option
    Change-Id: Ibf8accccc0f99d0bb047613e2608e1d086d5f2df

--- src/golang.conradwood.net/go-easyops/tokens/tokens.go
+++ src/golang.conradwood.net/go-easyops/tokens/tokens.go
@@ -22,6 +22,7 @@ var (
 	tokenwasread = false
 	usertoken    string
 	cloudname    = "yacloud.eu"
+	debug        = flag.Bool("ge_debug_tokens", false, "debug user token stuff")
 )
 
 const (
@@ -127,7 +128,9 @@ func readToken(token string) string {
 		// services usually don't have such file. don't throw errors in that case. it's confusing
 		return ""
 	}
-
+	if *debug {
+		fmt.Printf("[go-easyops] Reading file \"%s\", parsing as token\n")
+	}
 	btok, err = ioutil.ReadFile(fname)
 	if err != nil {
 		fmt.Printf("[go-easyops] Failed to read user token: %s\n", err)
@@ -141,7 +144,14 @@ func readToken(token string) string {
 	}
 	tok = strings.TrimSpace(tok)
 	//	fmt.Printf("[go-easyops] read token from \"%s\"\n", fname)
-
+	if *debug {
+		fmt.Printf("[go-easyops] token is empty\n")
+		if len(tok) < 5 {
+			fmt.Printf("[go-easyops] token is too short\n")
+		} else {
+			fmt.Printf("[go-easyops] token: %s...\n", tok[:3])
+		}
+	}
 	return tok
 }
 
