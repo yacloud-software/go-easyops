dae41ac6c79b4d13904274389848caaefd7379a2
Conrad Wood <cnw@conradwood.net>
2023-08-02 12:11:10 +0100

    automatic commit
    Change-Id: I3c5b9d90368fd9f71f07e344b59f397935c4a7bc

--- src/golang.conradwood.net/go-easyops/linux/dir_size.go
+++ src/golang.conradwood.net/go-easyops/linux/dir_size.go
@@ -5,7 +5,15 @@ import (
 	"os"
 )
 
+// if dir is the name of a directory, it will recursively calculate the size. if it is a file, it will stat it and return the filesize
 func DirSize(dir string) (uint64, error) {
+	fi, err := os.Stat(dir)
+	if err != nil {
+		return 0, err
+	}
+	if !fi.IsDir() {
+		return uint64(fi.Size()), nil
+	}
 	res := uint64(0)
 	utils.DirWalk(dir, func(root, relfile string) error {
 		fname := root + "/" + relfile
