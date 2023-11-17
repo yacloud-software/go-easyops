29c7237b94c50c1e23d65f6243c0bb76cf5c6089
Conrad Wood <cnw@conradwood.net>
2023-11-17 23:50:35 +0000

    added ctx serialise test
    Change-Id: I4ed7f8dc0208329406e1e83ab600b42a58fe75fe

--- src/golang.conradwood.net/tests/ctx/test-ctx.go
+++ src/golang.conradwood.net/tests/ctx/test-ctx.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"golang.conradwood.net/go-easyops/utils"
 	"os"
 )
 
@@ -14,10 +15,15 @@ const (
 var (
 	run_server = flag.Bool("server", false, "if true run server, othwerise client")
 	test_html  = flag.Bool("test_html", false, "if true do a test html render")
+	ser        = flag.Bool("serialise", false, "serialiser test")
 )
 
 func main() {
 	flag.Parse()
+	if *ser {
+		utils.Bail("failed to serialise", serialise())
+		os.Exit(0)
+	}
 	if *run_server {
 		start_server()
 	} else if *test_html {
