eb4c84af7561b14afff022aecb4a8058f5a9ec16
Conrad Wood <cnw@conradwood.net>
2024-11-14 15:13:56 +0000

    also added add uint32
    Change-Id: I7e0d2b1660265b0dd04cf4789e6744760b9fe0d0

--- src/golang.conradwood.net/go-easyops/utils/shifter.go
+++ src/golang.conradwood.net/go-easyops/utils/shifter.go
@@ -75,6 +75,21 @@ func (sh *Shifter) Unshift_uint8() uint8 {
 	sh.consumed_bytes++
 	return res
 }
+
+// modify the underlying byte array to set a uint32 at a particular address
+func (sh *Shifter) SetUint32(pos int, b uint32) {
+	if pos+4 >= len(sh.buf) {
+		sh.err = fmt.Errorf("Write beyond length (length=%d, write=%d)", len(sh.buf), pos)
+		return
+	}
+	sh.buf[pos] = byte(0xFF & b)
+	sh.buf[pos+1] = byte(0xFF & (b >> 8))
+	sh.buf[pos+2] = byte(0xFF & (b >> 16))
+	sh.buf[pos+3] = byte(0xFF & (b >> 24))
+}
+func (sh *Shifter) Bytes() []byte {
+	return sh.buf
+}
 func (sh *Shifter) Error() error {
 	return sh.err
 }
