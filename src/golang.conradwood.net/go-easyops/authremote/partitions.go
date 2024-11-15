5a1c7a29106590932458c14a8cf09566460d8639
Conrad Wood <cnw@conradwood.net>
2024-11-15 17:18:07 +0000

    automatic commit
    Change-Id: I2841f2eff561cb55998a04c460a7afe4e5f1dbdf

--- src/golang.conradwood.net/go-easyops/authremote/partitions.go
+++ src/golang.conradwood.net/go-easyops/authremote/partitions.go
@@ -17,7 +17,9 @@ a partition is either:
 the information in the context determines which partition will be returned
 a context without user information will always use partition 0
 
-Note: currently this is a bit of a stub. it only resolves the userid to a partition and does not consider the organisation
+	Note: currently this is a bit of a stub. it only resolves the userid to a partition and does not consider the organisation
+
+	PartitionIDs start from 100 upwards. this is meant to make it easier for tools to support custom partitions, such as "any user" or "no user" or so
 */
 func PartitionID(ctx context.Context) (uint64, error) {
 	u := auth.GetUser(ctx)
