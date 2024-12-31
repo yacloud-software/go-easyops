cd2c21b4713c407c9afed79e0f4535f1b4352b2f
Conrad Wood <cnw@conradwood.net>
2024-12-31 07:48:51 +0000

    disable pyroscope by default
    Change-Id: I01205b7ff75fb37f7890cab3eba1e2981dd74887

--- src/golang.conradwood.net/go-easyops/server/profiling.go
+++ src/golang.conradwood.net/go-easyops/server/profiling.go
@@ -1,21 +1,28 @@
 package server
 
 import (
+	"flag"
 	"fmt"
 	"github.com/grafana/pyroscope-go"
 	"golang.conradwood.net/go-easyops/appinfo"
 )
 
+var (
+	start_pyroscope = flag.Bool("ge_start_pyroscope", false, "use ge-pyroscope:3900 to record hotspots")
+)
+
 func start_profiling(sd *serverDef) {
-	pyroscope.Start(pyroscope.Config{
-		ApplicationName: sd.name,
-		ServerAddress:   "http://ge-pyroscope:3900",
-		Logger:          nil,
-		Tags: map[string]string{
-			"service_user_id": fmt.Sprintf("%s", sd.service_user_id),
-			"artefactid":      fmt.Sprintf("%d", appinfo.AppInfo().ArtefactID),
-			"version":         fmt.Sprintf("%d", appinfo.AppInfo().Number),
-		},
-	})
+	if *start_pyroscope {
+		pyroscope.Start(pyroscope.Config{
+			ApplicationName: sd.name,
+			ServerAddress:   "http://ge-pyroscope:3900",
+			Logger:          nil,
+			Tags: map[string]string{
+				"service_user_id": fmt.Sprintf("%s", sd.service_user_id),
+				"artefactid":      fmt.Sprintf("%d", appinfo.AppInfo().ArtefactID),
+				"version":         fmt.Sprintf("%d", appinfo.AppInfo().Number),
+			},
+		})
+	}
 
 }
