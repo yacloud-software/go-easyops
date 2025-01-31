9dad0c6aa7bfc4f7857ce631a06645993a4db3e8
Conrad Wood <cnw@conradwood.net>
2025-01-31 12:10:07 +0000

    sort of works
    Change-Id: If40b134d2d1efdb8319a8895704df4e806f0d28f

---
+++ src/golang.conradwood.net/tests/linuxcom/default_reader.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+type comDefaultReader struct {
+	pipe io.ReadCloser
+}
+
+func newDefaultReader(pipe io.ReadCloser) *comDefaultReader {
+	c := &comDefaultReader{pipe: pipe}
+	go c.read_loop()
+	return nil
+}
+func (r *comDefaultReader) Read([]byte) (int, error) {
+	return 0, nil
+}
+func (r *comDefaultReader) newBytes(buf []byte) error {
+	s := string(buf)
+	fmt.Print(s)
+	return nil
+}
+func (r *comDefaultReader) read_loop() {
+	buf := make([]byte, 8192)
+	var out_err error
+	for {
+		n, read_err := r.pipe.Read(buf)
+		if n != 0 {
+			xerr := r.newBytes(buf[:n])
+			if xerr != nil {
+				out_err = xerr
+				break
+			}
+		}
+		if read_err != nil {
+			if read_err != io.EOF {
+				out_err = read_err
+			}
+			break
+		}
+	}
+	if out_err != nil {
+		fmt.Printf("Error reading: %s\n", out_err)
+	}
+
+}
