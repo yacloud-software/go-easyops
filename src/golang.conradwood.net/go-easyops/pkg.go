0985719a7cc7735250f8aa51b8acb96f5ab57071
Conrad Wood <cnw@conradwood.net>
2025-06-30 19:40:58 +0100

    automatic commit
    Change-Id: Ib5938fc5257a3a636d5ce714b99198d16d471258

--- src/golang.conradwood.net/go-easyops/pkg.go
+++ src/golang.conradwood.net/go-easyops/pkg.go
@@ -1,7 +1,7 @@
 /*
 go-easyops provides an API for developers to create gRPC clients and servers in an easy-to-manage and scalable way.
 
-it intended to be used with the [yacloud].
+it is intended to be used with the [yacloud].
 
 The canonical URL for this documentation is [here].
 
