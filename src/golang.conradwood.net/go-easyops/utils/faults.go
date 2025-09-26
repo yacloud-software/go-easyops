ab9c4d193fafa3cfe1610c7c7e6c542c766237dd
Conrad Wood <cnw@conradwood.net>
2025-09-26 10:49:39 +0100

    automatic commit
    Change-Id: I166f9b34c0b31b81fbff43292c8629f7289070f1

--- src/golang.conradwood.net/go-easyops/utils/faults.go
+++ src/golang.conradwood.net/go-easyops/utils/faults.go
@@ -3,36 +3,50 @@ package utils
 import (
 	"context"
 	"fmt"
+	"sync"
+	"time"
 
 	"golang.yacloud.eu/apis/faultindicator"
 	"google.golang.org/grpc"
 )
 
 var (
+	fault_lock sync.Mutex
 	// public as implementation detail
 	Client_connector ClientConnector
+	faultcc          faultindicator.FaultIndicatorClient
 )
 
 type ClientConnector interface {
 	Connect(string) *grpc.ClientConn
 }
 
-// This logs a fault to the faultindicator service.
-func LogFault(ctx context.Context, name, desc string) {
+// get the faultindicator client.
+func GetFaultIndicatorClient() faultindicator.FaultIndicatorClient {
+	fault_lock.Lock()
+	defer fault_lock.Unlock()
+	if faultcc != nil {
+		return faultcc
+	}
 	cc := Client_connector
 	if cc == nil {
-		fmt.Printf("[go-easyops] *********** FAULT LOG: %s -> %s\n", name, desc)
-		return
+		fmt.Printf("No fault indicator client!\n")
+		time.Sleep(time.Duration(3) * time.Second)
+		return nil
 	}
 	con := cc.Connect("faultindicator.FaultIndicator")
-	fic := faultindicator.NewFaultIndicatorClient(con)
-	_, err := fic.LogFault(ctx, &faultindicator.LogFaultRequest{
+	faultcc = faultindicator.NewFaultIndicatorClient(con)
+	return faultcc
+}
+
+// This logs a fault to the faultindicator service.
+func LogFault(ctx context.Context, name, desc string) {
+
+	_, err := GetFaultIndicatorClient().LogFault(ctx, &faultindicator.LogFaultRequest{
 		Name:        name,
 		Description: desc,
 	})
 	if err != nil {
 		fmt.Printf("[go-easyops] --- failed to log fault: %s\n", ErrorString(err))
 	}
-	con.Close()
-
 }
