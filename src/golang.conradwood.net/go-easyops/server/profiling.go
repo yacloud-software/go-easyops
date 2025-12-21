8efa5ce27a22c89e8dd0d1ac1f1f54f617237bb1
Conrad Wood <cnw@conradwood.net>
2025-12-21 05:31:10 +0000

    automatic commit
    Change-Id: I30cc7b32031f50dae358f766c62c8e478b02fb8b

--- src/golang.conradwood.net/go-easyops/server/profiling.go
+++ src/golang.conradwood.net/go-easyops/server/profiling.go
@@ -3,26 +3,35 @@ package server
 import (
 	"flag"
 	"fmt"
+
 	"github.com/grafana/pyroscope-go"
 	"golang.conradwood.net/go-easyops/appinfo"
 )
 
+const (
+	pyroscope_port = 3900
+)
+
 var (
 	start_pyroscope = flag.Bool("ge_start_pyroscope", false, "use ge-pyroscope:3900 to record hotspots")
+	pyroscope_host  = flag.String("ge_pyroscope_host", "ge-pyroscope", "use `hostname`:3900 to record hotspots")
 )
 
 func start_profiling(sd *serverDef) {
 	if *start_pyroscope {
+		pyro_url := fmt.Sprintf("http://%s:%d", *pyroscope_host, pyroscope_port)
 		pyroscope.Start(pyroscope.Config{
 			ApplicationName: sd.name,
-			ServerAddress:   "http://ge-pyroscope:3900",
-			Logger:          nil,
+			ServerAddress:   pyro_url,
+			//			Logger:          nil,
+			Logger: pyroscope.StandardLogger,
 			Tags: map[string]string{
 				"service_user_id": fmt.Sprintf("%s", sd.service_user_id),
 				"artefactid":      fmt.Sprintf("%d", appinfo.AppInfo().ArtefactID),
 				"version":         fmt.Sprintf("%d", appinfo.AppInfo().Number),
 			},
 		})
+		fmt.Printf("[go-easyops] pyroscope started, connecting to host %s\n", pyro_url)
 	}
 
 }
